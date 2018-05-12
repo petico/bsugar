aa277a789bbc3adae731b4242690aac240943dc1
Petico <Petico@peticorp.com>
2018-05-13 06:50:05 +0900

    ヘルパ フラグとボタン表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def disp_flg(value)
+        "◎" if true == value
+    end
+    def icon_btn(icon, value="")
+        "<span class='oi #{icon}'>#{value}</span>".html_safe
+    end
+
 end
